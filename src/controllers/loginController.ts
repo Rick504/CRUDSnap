696f547aa0fff0581653851e9ea6f68ca442cf76
Rick Wesley <rick.wes1@gmail.com>
2023-04-06 16:09:36 -0300

    criando uma camada de segunrança e separando função de criação de token

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,16 +1,11 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { setToken } from '../security/auth';
 
-const loginController = (req: Request, res: Response) => {
-  const jwtSecret = process.env.JWT_SECRET as string;
-  const userDataJWT = req.body;
-  jwt.sign({ userDataJWT }, jwtSecret, { expiresIn: '30m' }, (err, token) => {
-    if (err) {
-      res.status(500).send('Erro ao gerar token.');
-      return;
-    }
+const loginController = async (req: Request, res: Response) => {
+  const user = req.body;
+  const { email } = user;
+  const token = await setToken(user);
 
-    res.json({ email: userDataJWT.email, auth: true, token });
-  });
+  res.json({ email, auth: true, token });
 };
 export default loginController;
